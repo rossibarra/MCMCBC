d09b60c880b6f032a55e0609698d247c15f816f8
Jeff Ross-Ibarra <rossibarra@gmail.com>
2017-12-08 10:08:36 -0800

    libs again

--- install.R
+++ install.R
@@ -4,10 +4,6 @@ new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"
 if(length(new.packages)) install.packages(new.packages)
 lapply(list.of.packages, require, character.only = TRUE)
 
-library(gsl)
-library(utils)
-library(dplyr)
-library(coda)
 
 #Colors
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
