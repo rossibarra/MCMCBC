bc174b91c823a916d4d488b516a49a84635a2b0c
Jeff Ross-Ibarra <rossibarra@gmail.com>
2017-12-07 23:17:22 -0800

    nope none

--- install.R
+++
@@ -1,8 +0,0 @@
-#Packages
-list.of.packages <- c("gsl","coda","utils","dplyr","gsr")
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
-
-#Colors
-cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
-
