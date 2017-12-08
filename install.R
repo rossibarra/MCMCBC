3be6c54344dcff91e74d4998e57845cb43f6a85a
Jeff Ross-Ibarra <rossibarra@gmail.com>
2017-12-07 23:36:47 -0800

    nogsr

--- install.R
+++ install.R
@@ -1,5 +1,5 @@
 #Packages
-list.of.packages <- c("gsl","coda","utils","dplyr","gsr")
+list.of.packages <- c("gsl","coda","utils","dplyr")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 lapply(list.of.packages, require, character.only = TRUE)
